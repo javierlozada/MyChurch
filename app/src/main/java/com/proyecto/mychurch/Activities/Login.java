dd1e34e6fd16b87cb29f4c37aa5c85b750f7eeb7
djl <devin.lozada@barbersapp.co>
2020-09-13 12:49:07 -0500

    First Commit

---
+++ app/src/main/java/com/proyecto/mychurch/Activities/Login.java
@@ -0,0 +1,16 @@
+package com.proyecto.mychurch.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyecto.mychurch.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
